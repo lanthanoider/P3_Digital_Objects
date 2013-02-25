0128ccdc72ede2e76ea0a6bdd3817294b0e03994
Joel Clark <jclarkvr@gmail.com>
2013-02-25 18:41:49 +0000

    First Commit

---
+++ P3_Digital_objects/P3_Digital_objects/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  P3_Digital_objects
+//
+//  Created by Joel Clark on 25/02/2013.
+//  Copyright (c) 2013 Joel Clark. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
